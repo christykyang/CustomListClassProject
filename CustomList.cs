6162b425cd82ddf27f898af55a71563160ca4e0d
unknown <christykyang@gmail.com>
2020-02-24 20:18:56 -0600

    Count works! First Add UnitTest also passed. Will currently need to work on indexer to test second AddMethod. YOU ARE BEHIND. Get working and/or ask for help, please and thanks!

--- CustomList.cs
+++ CustomList.cs
@@ -8,13 +8,48 @@ namespace CustomListProject
 {
     public class CustomList<T>
     {
+        //member variables
+        private int count;
+        private int indexLocation;
+        private int maxCapacity;
+        //constructors
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                count = 0;
+            }
+        }
+        public int IndexLocation
+        {
+            get
+            {
+                return indexLocation;
+            }
+            set
+            {
+                indexLocation = -1;
+            }
+        }
+        //public int MaxCapacity
+        //{
+            
+        //}
+        //member methods
         public void AddItem(T item)
         {
             //increment count
+            count++;
 
             //item needs to land at next available index
+            //indexLocation++;
 
             //check to make sure it persists
+
 
             //count == maxcapacity, we need to increase capacity and copy
         }
